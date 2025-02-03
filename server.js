2a5dfffd9eb3a4d1799f2456139a44e48055c80a
haast7 <erick@haast.com.br>
2025-02-03 12:09:48 -0300

    Ajuste para deploy

--- server.js
+++ server.js
@@ -68,8 +68,8 @@ app.use((err, req, res, next) => {
 });
 
 // Iniciar o servidor
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
     console.log(`Diretório das taxas: ${PARCEIROS_DIR}`);
 });
